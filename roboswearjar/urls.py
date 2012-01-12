6243331a6058ce77ba9d9916a5dce52c9213bc48
Ian Adam Naval <ianonavy@gmail.com>
2012-01-11 19:24:14 -0800

    Configured URLs

--- roboswearjar/urls.py
+++ roboswearjar/urls.py
@@ -1,17 +1,9 @@
+#!/usr/bin/env python
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'roboswearjar.views.home', name='home'),
-    # url(r'^roboswearjar/', include('roboswearjar.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('core.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
