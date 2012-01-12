37b6cc849614b596e8e1619d669114c9456fa15a
Ian Adam Naval <ianonavy@gmail.com>
2012-01-11 17:51:13 -0800

    Add models and plan views.

--- roboswearjar/core/models.py
+++ roboswearjar/core/models.py
@@ -1,3 +1,79 @@
+#!/usr/bin/env python
+
+""" Django models file to define the models for the swear jar application.
+
+Contains code to encapsulate a swear type, a particular swear, and an
+individual RoboKnights team member.
+
+"""
+
 from django.db import models
 
-# Create your models here.
+__author__ = "Ian Adam Naval"
+__copyright__ = "Copyright 2012 Ian Adam Naval"
+
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Ian Adam Naval"
+__email__ = "ianonavy@gmail.com"
+__status__ = "Development"
+
+
+class SwearType(models.Model)
+    """ Model that encapsulates a particular swear type. Instances of these are
+    meant to be created in the Django Administration page."""
+
+    phrase = models.CharFiel(max_length=255)
+    value = models.FloatField()
+
+    def __unicode__(self):
+        return self.phrase
+
+class Swear(models.Model):
+    """ Model that encapsulates a specific instance of a swear. These are 
+    generated and saved to the database by the a Knight's add_swear method. A
+    Swear has one type and one speaker. """
+
+    swear_type = models.ForeignKey(SwearType)
+    speaker = models.ForeignKey(Knight)
+
+    def getValue(self):
+        return self.swear_type.value
+
+    def __unicode__(self):
+        return "%s said %s." % (self.speaker, self.swear_type)
+
+
+class Knight(models.Model):
+    """ Model that encapsulates a specific member of Team RoboKnights (e.g. a
+    Knight). A Knight has many swears.""" 
+
+    name = models.CharField(max_length=255)
+
+    def debt_for_type(self, swear_type):
+        """ Returns the debt for a particular type of swear. """
+
+        debt = 0
+        for swear in Swear.objects.filter(speaker=self, swear_type=swear_type):
+            debt = debt + swear.getValue()
+        return debt
+
+    def total_debt(self):
+        """ Returns the total debt for all types of swears. """
+
+        debt = 0
+        for swear in Swear.objects.filter(speaker=self):
+            debt = debt + swear.getValue()
+        return debt
+
+    def add_swear(self, swear_type):
+        """ Adds a new swear to the database with this Knight as its
+        speaker. """
+        
+        swear = Swear()
+        swear.sweartype = swear_type
+        swear.speaker = self
+        swear.save()
+
+    def __unicode__(self):
+        return self.name
--- roboswearjar/core/views.py
+++ roboswearjar/core/views.py
@@ -1,1 +1,24 @@
-# Create your views here.
+#!/usr/bin/env python
+
+"""Django views for the bookmarket application."""
+
+from utils import load_page
+
+__author__ = "Ian Adam Naval"
+__copyright__ = "Copyright 2011 ian Adam Naval"
+__credits__ = []
+
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Ian Adam Naval"
+__email__ = "ianonavy@gmail.com"
+__status__ = "Development"
+__date__ = "17 October 2011"
+
+def index(request):
+
+def login(request):
+
+def add_knight(request):
+
+def add_swear(request):
