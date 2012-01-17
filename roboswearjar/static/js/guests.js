7a1a3b66d9b00668c795bff64ba2e76db7c8a026
Ian Adam Naval <ianonavy@gmail.com>
2012-01-17 02:32:18 -0800

    Moved jar on top of coins.

--- roboswearjar/static/js/guests.js
+++ roboswearjar/static/js/guests.js
@@ -4,16 +4,12 @@ var drawingCanvas = document.getElementById('myDrawing');
 if (drawingCanvas.getContext) {
     // Initaliase a 2-dimensional drawing context
     var context = drawingCanvas.getContext('2d');
-
-    // Create the yellow face
+    
+    // Set the coin fill and stroke.
     context.strokeStyle = "#000000";
     context.fillStyle = "#FFFF00";
-    var myImage = new Image();
-        myImage.onload = function() {
-        context.drawImage(myImage, 0, 0, 175, 256);
-    }
-    myImage.src = "http://roboswearjar.ep.io/static/img/jar.png";
     
+    // Max Y values for coins in the jar depending on the x value.
     var maxYs = new Object();
     maxYs[12] = 212;
     maxYs[22] = 222;
@@ -30,29 +26,48 @@ if (drawingCanvas.getContext) {
     maxYs[132] = 212;
     
     var dollars = 0;
+    // Use AJAX to get the number of dollars in the swear jar.
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange  = function() { 
         if (xhr.readyState == 4) {
             dollars = parseInt(xhr.responseText);
+            // Draw a coin for each dollar in the swear jar.
             for (var i = 0; i < dollars; i++) {
                 x = generateX();
-                drawCoin(context, x, generateY(x, i, maxYs));
+                drawCoin(context, x, generateY(x, maxYs));
             }
-         }
+            
+            // Draw the jar on top of the coins.
+            var myImage = new Image();
+                myImage.onload = function() {
+                context.drawImage(myImage, 0, 0, 175, 256);
+            }
+            myImage.src = "http://roboswearjar.ep.io/static/img/jar.png";
+        }
     }; 
     xhr.open("GET", "http://roboswearjar.ep.io/total/", true);
     xhr.send();
 }
 
+/**
+ * Generates a random number from a number to another number at a certain interval.
+ */
 function random(from, to, interval) {
     return Math.floor(Math.random() * (to / interval - from / interval + 1) + from / interval) * interval;
 }
 
+/**
+ * Generates a random pixel value between 12 and 132 at an interval of 10.
+ */
 function generateX() {
     return random(10, 130, 10) + 2;
 }
 
-function generateY(x, h, maxYs) {
+/**
+ * Generates the y value for the coin. Subtracting from the array of stored
+ * y values per x-value allows for a stacking effect to be created.
+ */
+function generateY(x, maxYs) {
     y = maxYs[x];
     if (maxYs[x] > 62) {
         maxYs[x] -= 10;
@@ -60,33 +75,36 @@ function generateY(x, h, maxYs) {
     return y;
 }
 
+/**
+ * Canvas function to actually draw the coin.
+ */
 function drawCoin(ctx, x, y) {
-  var kappa = .5522848,
-      w = 30,
-      h = 15,
-      k = h / 2.5;
-      ox = (w / 2) * kappa, // offset horizontal
-      oy = (h / 2) * kappa, // offset vertical
-      xe = x + w,           // x-end
-      ye = y + h,           // y-end
-      xm = x + w / 2,       // x-middle
-      ym = y + h / 2;       // y-middle
-      yt = y - k;
-      yet = ye - k;
-      ymt = ym - k
+    var kappa = .5522848,
+        w = 30,
+        h = 15,
+        k = h / 2.5;
+        ox = (w / 2) * kappa, // offset horizontal
+        oy = (h / 2) * kappa, // offset vertical
+        xe = x + w,           // x-end
+        ye = y + h,           // y-end
+        xm = x + w / 2,       // x-middle
+        ym = y + h / 2;       // y-middle
+        yt = y - k;
+        yet = ye - k;
+        ymt = ym - k
 
-  ctx.beginPath();
-  ctx.moveTo(xe, ym);
-  ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
-  ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
-  ctx.lineTo(x, ymt);
-  ctx.bezierCurveTo(x, ymt - oy, xm - ox, yt, xm, yt);
-  ctx.bezierCurveTo(xm + ox, yt, xe, ymt - oy, xe, ymt);
-  ctx.fill();
-  ctx.bezierCurveTo(xe, ymt + oy, xm + ox, yet, xm, yet);
-  ctx.bezierCurveTo(xm - ox, yet, x, ymt + oy, x, ymt);
-  ctx.moveTo(xe, ymt);
-  ctx.lineTo(xe, ym);
-  ctx.stroke();
-  ctx.closePath();
+    ctx.beginPath();
+    ctx.moveTo(xe, ym);
+    ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+    ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+    ctx.lineTo(x, ymt);
+    ctx.bezierCurveTo(x, ymt - oy, xm - ox, yt, xm, yt);
+    ctx.bezierCurveTo(xm + ox, yt, xe, ymt - oy, xe, ymt);
+    ctx.fill();
+    ctx.bezierCurveTo(xe, ymt + oy, xm + ox, yet, xm, yet);
+    ctx.bezierCurveTo(xm - ox, yet, x, ymt + oy, x, ymt);
+    ctx.moveTo(xe, ymt);
+    ctx.lineTo(xe, ym);
+    ctx.stroke();
+    ctx.closePath();
 }
