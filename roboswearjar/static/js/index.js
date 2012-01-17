d8af657325c0faadb20acbba99e02396910c3600
Ian Adam Naval <ianonavy@gmail.com>
2012-01-16 23:03:56 -0800

    Fixed JS (again).

--- roboswearjar/static/js/index.js
+++ roboswearjar/static/js/index.js
@@ -1,18 +1,18 @@
 function show(e) {
-    var debt = e.currentTarget.childNodes[2];
+    var debt = e.currentTarget.childNodes[3];
     debt.style.display = "block";
 }
-
+ 
 function hide(e) {
-    var debt = e.currentTarget.childNodes[2];
+    var debt = e.currentTarget.childNodes[3];
     debt.style.display = "none";
 }
-
+ 
 var knights = document.getElementsByClassName("knight");
-console.log(knights);
 for (i in knights) {
     var knight = knights[i];
-    var debts = knight.childNodes[2];
+    var debts = knight.childNodes[3];
+    console.log(knight.childNodes);
     knight.addEventListener("mouseover", show, false);
     knight.addEventListener("mouseout", hide, false);
     debts.style.display = "none";
